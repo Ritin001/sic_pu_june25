80093ca975dffec840734d1cbc5a4f7a32ad8347
ritin_kumar_gupta <ritin15032006@gmail.com>
2025-07-17 09:12:32 +0530

    assignment done

--- assignment/sorting/bubble_sort.py
+++
@@ -1,6 +0,0 @@
-given_list = list(map(int, input("Enter the numbers separated by space: ").split()))
-for i in range(len(given_list) - 1):
-    for j in range(len(given_list) - 1 - i):
-        if given_list[j] > given_list[j + 1]:
-            given_list[j], given_list[j + 1] = given_list[j + 1], given_list[j]
-print("Sorted list:", given_list)
